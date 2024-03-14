146c53e5b50011858502cb832cd03b51cec58b30
Jan Sobolewski <59810745+Jassobolewski@users.noreply.github.com>
2024-03-14 16:06:09 -0400

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.group47.canadadash {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.group47.canadadash to javafx.fxml;
+    exports com.group47.canadadash;
+}
