c57d1cffca1c5280a4f54c00128519a1a0dc1713
Muhammad Affan Yasir <myasir2@uwo.ca>
2024-03-25 18:48:07 +0000

    module-info.java edited online with Bitbucket
    forgot to configure the dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.group.canadadash {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires org.json;
+    requires com.google.gson;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
