fdf823dcc71841a7ea40d807f9bac00bae243e70
Sara Lahourpour <slahourp@uwo.ca>
2024-03-26 11:10:07 -0400

    Modified class Leaf, and added its JavaDoc

--- src/main/java/com/group47/canadadash/Leaf.java
+++ src/main/java/com/group47/canadadash/Leaf.java
@@ -1,66 +1,45 @@
 package com.group47.canadadash;
-
-import java.util.ArrayList;
-
+import com.group47.canadadash.processing.Question;
+/**
+ * This class represents a leaf object in the game, which consists of the
+ * question itself, the right answer, hints, and options
+ * leaf has both x, and y location
+ * width, and height dimension
+ * and an image address pointing to its relevant img file
+ */
 public class Leaf extends GameUnit {
 
-    private String question;
-    private ArrayList<String> options;
-    private ArrayList<String> hints;
-    private String rightAnswer;
-
+    private Question ques;
     private int points;
 
+    /**
+     * Initializing the height, width and imageAdd of leaf
+     * @param givenX the x location to be set for leaf
+     * @param givenY the y location to be set for leaf
+     */
     public Leaf(int givenX, int givenY) {
         super(givenX, givenY);
-        question = "";
-        rightAnswer = "";
-        options = new ArrayList<>();
-        hints = new ArrayList<>();
-        points = 0;
+        ques = new Question("", "", "");
+        points = 100;
         height = 20;
         width = 20;
-        imageAdd = "";    //TODO: write the address which points to the leaf image
+        imageAdd = "src/main/resources/com/group47/canadadash/leaf.png";
     }
 
-    public void setQuestion(String givenQuestion) {
-        question = givenQuestion;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void addOptions(String newOption){
-        options.add(newOption);
-    }
-
-    public ArrayList<String> getOptions() {
-        return options;
-    }
-
-    public void addHint(String newHint){
-        hints.add(newHint);
-    }
-
-    public ArrayList<String> getHints() {
-        return hints;
-    }
-
-    public void setRightAnswer(String rightAnswer) {
-        this.rightAnswer = rightAnswer;
-    }
-
-    public String getRightAnswer() {
-        return rightAnswer;
-    }
-
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
+    /**
+     * getter of attribute points
+     * @return points
+     */
     public int getPoints() {
         return points;
+    }
+
+    /**
+     * so it can modify later
+     * @return the question object
+     */
+    public Question getQues() {
+        return ques;
     }
 
 }
