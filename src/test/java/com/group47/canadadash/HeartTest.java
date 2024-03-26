4e77f6296c4460a3fec509a13ff7575036058acd
Sara Lahourpour <slahourp@uwo.ca>
2024-03-26 10:33:59 -0400

    Empty Test Classes

---
+++ src/test/java/com/group47/canadadash/HeartTest.java
@@ -0,0 +1,62 @@
+package com.group47.canadadash;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeartTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getX() {
+    }
+
+    @Test
+    void getY() {
+    }
+
+    @Test
+    void setX() {
+    }
+
+    @Test
+    void setY() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void getWidth() {
+    }
+
+    @Test
+    void getImageAdd() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void setHeart() {
+    }
+
+    @Test
+    void isFullHeart() {
+    }
+
+    @Test
+    void setImageAdd() {
+    }
+}
