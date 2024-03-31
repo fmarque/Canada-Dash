0fffa4e4888419aebef8c231aa20327af09a1acd
Anne <azhan67@uwo.ca>
2024-03-31 00:36:53 -0400

    testapplication for java

---
+++ src/main/java/com/group47/canadadash/game/testapplication.java
@@ -0,0 +1,25 @@
+package com.group47.canadadash.game;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class testapplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(testapplication.class.getResource("01_settings.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.setMaxHeight(720);
+        stage.setMaxWidth(1280);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
