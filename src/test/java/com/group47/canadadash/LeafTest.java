993b8b914c25a3768c98e0758ce30077cbebc0f5
Sara Lahourpour <slahourp@uwo.ca>
2024-03-31 22:23:24 -0400

    Added LeafTest

--- src/test/java/com/group47/canadadash/LeafTest.java
+++ src/test/java/com/group47/canadadash/LeafTest.java
@@ -1,5 +1,6 @@
 package com.group47.canadadash;
 
+import com.group47.canadadash.processing.Question;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,9 +9,18 @@ class LeafTest {
 
     @Test
     void getPoints() {
+        System.out.println("getPoints");
+        Leaf leaf = new Leaf(50, 50);
+        int expResult = 100;
+        int result = leaf.getPoints();
+        assertEquals(expResult, result);
     }
 
     @Test
     void getQues() {
+        System.out.println("getQues");
+        Leaf leaf = new Leaf(50, 50);
+        Question question = new Question("", "", "");
+//        assertEquals();
     }
 }
