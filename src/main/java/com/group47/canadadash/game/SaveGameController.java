6573cfb4bdaf9473fbff7f1c42fa583688beef43
Jan Sobolewski <59810745+Jassobolewski@users.noreply.github.com>
2024-04-01 18:28:48 -0400

    Save progress fixed

--- src/main/java/com/group47/canadadash/game/SaveGameController.java
+++ src/main/java/com/group47/canadadash/game/SaveGameController.java
@@ -1,20 +1,26 @@
 package com.group47.canadadash.game;
 
+import com.group47.canadadash.processing.App;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SaveGameController {
 
-    //    @FXML
-//    private void handelSaveBtn(){
-//
-//    }
-//    }
+    @FXML
+    private void handelSaveBtn(ActionEvent event){
+        App.getInstance().userSave();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
     @FXML
     private void handelCancelBtn() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/01_settings.fxml"));
